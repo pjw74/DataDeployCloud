370c89d9dab57d854c46c86407032d4a9c426497
pjw74 <70009161+pjw74@users.noreply.github.com>
2023-06-07 04:18:12 +0900

    Mod trend_upload.py

--- trend_upload.py
+++ trend_upload.py
@@ -5,9 +5,15 @@ import datetime
 from google.cloud import storage
 import time
 from google.auth.exceptions import RefreshError
-from google.cloud.exceptions import NotFound
 import requests
+# import sys
+# !{sys.executable} -m pip install requests
 import json
+from pathlib import Path
+from google.cloud.exceptions import Conflict, NotFound
+from io import BytesIO
+import pytz
+from datetime import datetime
 
 # 인증 정보 설정
 credentials_path = '/home/rsa-key-20230529/Github/job-posting-api-388303-0fa2808b9dcb.json'
@@ -87,23 +93,55 @@ def upload_file_to_gcs(file_path, bucket_name, destination_blob_name):
         print(f"An error occurred while uploading {file_path}")
         print(str(e))
 
+def blob_dir_creator(project_id, bucket_name, blob_dir):
+    """ 경로를 의미하는 str 변수를 인자로 받아 버킷의 경로를 생성함 """
+    blob_dir = Path(blob_dir)
+    directory_blob = ''
+    for component in blob_dir.parts:
+        try:
+            client = storage.Client(project=project_id)
+            bucket = client.bucket(bucket_name)
+            directory_blob = "".join([directory_blob, component])
+            blob = bucket.blob(directory_blob + "/")
+            blob.upload_from_string("")
+        except Conflict:
+            print(f"{directory_blob} already existed")
+        except NotFound:
+            print(f"{bucket_name} not found")
+            return None
+    return directory_blob
+
 def merge_and_upload_data(df_list, project_id, bucket_name):
     """ 전체 결과를 만들기 위한 함수 """
 
     try:
-        # DataFrame 리스트를 합치기
+        # DataFrame 리스트를 생성하여 데이터 프레임에 추가하기
         merged_df = pd.concat(df_list, axis=1)
-        # 결과 CSV 파일을 저장하기 위한 Cloud Storage 정보
-        ts = datetime.datetime.now()
-        destination_blob_name = 'google_trend/result_{}.csv'.format(ts)
-        # 결과 파일을 CSV식으로 저장 (Cloud Storage 버킷에 업로드)
-        merged_df.to_csv(destination_blob_name, index=False)
-        upload_file_to_gcs(destination_blob_name, bucket_name, destination_blob_name)
-        print(f"DataFrame uploaded to {destination_blob_name} in {bucket_name}")
+        # CSV 형식의 문자열로 저장하기
+        csv_string = merged_df.to_csv(index=False)
+
+        # 버킷에 새로운 디렉토리 생성 (하위 버킷이 필요한 경우 추가)
+        directory_blob = blob_dir_creator(project_id, bucket_name, 'google_trend/')
+
+
+        # KST 타임존으로 변경
+        KST = pytz.timezone('Asia/Seoul')
+        now = datetime.now(KST).strftime('%Y-%m-%d %H:%M:%S.%f')
+        # 경로 생성
+        blob_name = f"{directory_blob}/result_{now}.csv"
+
+        # 업로드할 전체 경로 지정
+        client = storage.Client()
+        bucket = client.get_bucket(bucket_name)
+        blob = bucket.blob(blob_name)
+
+        # 파일 업로드
+        blob.upload_from_string(data=csv_string, content_type='text/csv')
+        print(f"Data uploaded to {blob_name} in {bucket_name}")
 
         # Slack에 알림 메시지 전송
         num_data = len(df_list)
-        send_slack_notification("Google Trend 데이터 수집이 완료되었습니다.", num_data)
+        # send_slack_notification("Google Trend 데이터 수집이 완료되었습니다.", num_data)
 
     except Exception as e:
         print(f"An error occurred while uploading to {bucket_name}")
